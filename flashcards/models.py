6df92f76bd66c035236174931cd9979ac3711701
sihsob <bcsihsobhon@gmail.com>
2015-04-18 15:32:51 -0400

    Add url field and database creation

--- flashcards/models.py
+++ flashcards/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Cards(models.Model):
+class flashcard(models.Model):
 	title = models.CharField(max_length=200)
-	vocabulary = models.CharField()
+	vocabulary = models.CharField(max_length=10000)
 	post_date = models.DateTimeField('date posted')
+	url = models.CharField(max_length=32)
