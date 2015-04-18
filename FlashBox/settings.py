acf74366f3b2e59eb6e1ccc0b588895645d77301
Toshi Piazza <toshi.piazza@gmail.com>
2015-04-18 13:48:33 -0400

    Adds projects structure, templates and static files.

--- FlashBox/settings.py
+++ FlashBox/settings.py
@@ -1,5 +1,7 @@
 # Django settings for FlashBox project.
 
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -70,6 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join('static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -109,6 +112,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.dirname('templates'),
 )
 
 INSTALLED_APPS = (
