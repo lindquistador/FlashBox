5f862641413739a0261a9f0c467cfc59b9b0e3af
Toshi Piazza <toshi.piazza@gmail.com>
2015-04-18 16:12:41 -0400

    Adds form GET/POST to main project

--- FlashBox/urls.py
+++ FlashBox/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'FlashBox.views.home', name='home'),
+    url(r'^(?P<url>\w', 'FlashBox.views.view_cards', name='view_cards'),
     # url(r'^FlashBox/', include('FlashBox.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- FlashBox/views.py
+++ FlashBox/views.py
@@ -1,7 +1,30 @@
 from django.http import Http404
 from django.shortcuts import render_to_response
+from .forms import UploadFileForm
 
+def handle_uploaded_file(f):
+    """ Helper function for home.
+        Sends the file to the database after parsing
+        and returns the automagically generated url """
+    pass
+
+# TODO: implement csrf?
 def home(request):
+    """ Handles the home page and the upload handling. """
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            url = handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/'+url)
+    else:
+        form = UploadFileForm()
     return render_to_response('home', {
-            "title": 'FlashBox'
+            'title': 'FlashBox',
+            'form': form
         })
+
+def view_cards(request, url):
+    """ Searches the database for a particular
+        url which corresponds to their cards and
+        renders them. """
+    pass
