d4b9a5ca94eb539dd8bace564e51ac0fab01354a
Toshi Piazza <toshi.piazza@gmail.com>
2015-04-18 17:58:23 -0400

    Small housekeeping

--- FlashBox/settings.py
+++ FlashBox/settings.py
@@ -13,12 +13,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(os.path.dirname(os.path.realpath(__file__)), 'db'), # Or path to database file if using sqlite3.
-        'USER': '',                             # Not used with sqlite3.
-        'PASSWORD': '',                         # Not used with sqlite3.
-        'HOST': '',                             # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                             # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(
+                    os.path.dirname(
+                            os.path.realpath(__file__)
+                    ),
+                    'db'
+                ),
     }
 }
 
